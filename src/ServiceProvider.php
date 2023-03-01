7f64cf40da7c1949101c4e6177ec91423b1a76ea
Jack Sleight <jacksleight@gmail.com>
2023-03-01 15:11:25 +0000

    WIP

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,12 +2,7 @@
 
 namespace JackSleight\StatamicBardIpsum;
 
-use Illuminate\Support\Arr;
-use JackSleight\StatamicBardMutator\Facades\Mutator;
-use JackSleight\StatamicBardIpsum\Marks\Span;
-use Statamic\Fieldtypes\Bard\Augmentor;
 use Statamic\Providers\AddonServiceProvider;
-use Statamic\Statamic;
 
 class ServiceProvider extends AddonServiceProvider
 {
