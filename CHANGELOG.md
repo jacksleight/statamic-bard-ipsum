be6648d48f27ab3830118d17daf672d29ed2a105
Jack Sleight <jacksleight@gmail.com>
2023-05-02 11:30:48 +0100

    Statamic v4 Support (#1)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
 # Changelog
 
-## 0.1.1 (NEXT)
+## 1.0.0 (02-05-2023)
 
+- [new] Statamic 4 support
 - Rename core extension to camel case
+- Dropped support for Statamic 3
 
 ## 0.1.0 (2023-02-03)
 
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,16 +2,15 @@
 
 namespace JackSleight\StatamicBardIpsum;
 
-use Illuminate\Support\Arr;
-use JackSleight\StatamicBardMutator\Facades\Mutator;
-use JackSleight\StatamicBardIpsum\Marks\Span;
-use Statamic\Fieldtypes\Bard\Augmentor;
 use Statamic\Providers\AddonServiceProvider;
-use Statamic\Statamic;
 
 class ServiceProvider extends AddonServiceProvider
 {
-    protected $scripts = [
-        __DIR__.'/../dist/js/addon.js',
+    protected $vite = [
+        'hotFile' => __DIR__.'/../vite.hot',
+        'publicDirectory' => 'dist',
+        'input' => [
+            'resources/js/addon.js',
+        ],
     ];
 }
